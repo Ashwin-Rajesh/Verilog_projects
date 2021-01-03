9071a78cd151bb6d8ea77c4e788212e7923e0b5e
Ashwin <aswinrajesh1@gmail.com>
2021-01-04 00:00:01 +0530

    Implemeted signed adder using generate and array wires

--- multiplier/multiplier_tb.v
+++ multiplier/multiplier_tb.v
@@ -3,18 +3,24 @@ module multiplier_tb;
 
     reg[WIDTH - 1:0]        ina, inb;
     reg                     clk;
+    reg                     clk2;
     wire[2 * WIDTH - 1:0]   out;
     reg[2 * WIDTH - 1:0]    ref;
 
-    multiplier #(WIDTH) mod(ina, inb, clk, out);
+    integer i;
+
+    simple_signed_multiplier #(WIDTH) tud(ina, inb, clk, out);
 
     initial begin
         $monitor("%8d * %8d = %16d", ina, inb, out);
 
         $dumpfile("multiplier.vcd");
         $dumpvars(0, multiplier_tb);
+        for(i = 0; i < WIDTH; i = i + 1)
+            $dumpvars(0, tud.partial_sum[i]);    
 
-        clk = 0;
+        clk     = 0;
+        clk2    = 0;
 
         #200 $finish;
     end
@@ -29,5 +35,7 @@ module multiplier_tb;
         #1
         clk = 1;
     end
+
+    always #0.5 clk2 = ~clk2;
 
 endmodule
