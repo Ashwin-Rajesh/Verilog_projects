4f2b9c1e6935a8433ee2f48cba8b23e52e3f7718
Ashwin <aswinrajesh1@gmail.com>
2021-01-04 19:31:15 +0530

    Removed unnecessary begin block

--- multiplier/simple_unsigned_multiplier.v
+++ multiplier/simple_unsigned_multiplier.v
@@ -12,11 +12,9 @@ module simple_unsigned_multiplier(ina, inb, clk, out);
     assign partial_sum[0] = {(2*WIDTH){inb[0]}} & ina; 
     
     generate
-        begin
             for(i = 1; i < WIDTH; i = i + 1) begin : mult_stage
                 assign partial_sum[i] = partial_sum[i-1] + ({(2*WIDTH){inb[i]}} & (ina << i));
             end
-        end
     endgenerate
     
     always @(posedge clk) out = partial_sum[WIDTH-1];
