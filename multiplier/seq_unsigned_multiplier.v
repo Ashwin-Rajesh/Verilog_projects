32a2a1114c5c1e258aaf3f5766c56478b650e8ad
Ashwin <aswinrajesh1@gmail.com>
2021-01-04 19:35:59 +0530

    Changed temp to partial_product

--- multiplier/seq_unsigned_multiplier.v
+++ multiplier/seq_unsigned_multiplier.v
@@ -10,7 +10,7 @@ module seq_unsigned_multiplier(ina, inb, clk, start, out, ready);
     output reg[2*WIDTH-1: 0]    out;
     output reg                  ready;
 
-    reg[2*WIDTH - 1:0]          temp;
+    reg[2*WIDTH - 1:0]          partial_product;
     reg[4:0]                    bit;
     reg[2*WIDTH-1: 0]           multiplicand;
     reg[WIDTH-1: 0]             multiplier;
@@ -21,19 +21,19 @@ module seq_unsigned_multiplier(ina, inb, clk, start, out, ready);
             multiplicand <= ina;
             multiplier   <= inb;
             bit          <= WIDTH;
-            temp         <= 0;
+            partial_product         <= 0;
             ready        <= 1'b0;
         end
 
         if(bit != 4'b0) begin
             if(multiplier[0] == 1'b1)
-                temp = temp + multiplicand;
+                partial_product = partial_product + multiplicand;
             multiplicand = multiplicand << 1;
             multiplier   = multiplier >> 1;
             bit          = bit - 1;
 
             if(bit == 1'b0) begin
-                out = temp;
+                out = partial_product;
                 ready = 1'b1; 
             end
         end
