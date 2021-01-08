1122e88d12bbdd11df2feb26c018c8b6ec755b99
Ashwin <aswinrajesh1@gmail.com>
2021-01-08 16:11:28 +0530

    Unified read and write addresses

--- RiSC16/non_pipelined/mem_data.v
+++ RiSC16/non_pipelined/mem_data.v
@@ -10,19 +10,17 @@
 // data : dataOut   : Data read
 
 // Inputs
-// data : readAddr  : Address for reading
-// data : writeAddr : Address for writing
-// data : dataIn    : Data for writing
+// data : address   : Address for accessing data
+// data : dataIn    : Data to write
 // cntr : clk       : Clock signal
 // cntr : writeEn   : Enable for writing (active high)
 // cntr : rst       : Reset data to 0 (active high)
 
 
-module mem_data (dataOut, readAddr, writeAddr, dataIn, clk, writeEn, rst);
+module mem_data (dataOut, address, dataIn, clk, writeEn, rst);
     output[`WORD_LEN-1:0]       dataOut;    // Data for reading
     
-    input[`ADDR_LEN-1:0]        readAddr;   // Address for reading
-    input[`ADDR_LEN-1:0]        writeAddr;  // Address for writing
+    input[`ADDR_LEN-1:0]        address;    // Address of data
     input[`WORD_LEN-1:0]        dataIn;     // Data for writing
 
     input clk;                              // Clock signal
@@ -30,10 +28,8 @@ module mem_data (dataOut, readAddr, writeAddr, dataIn, clk, writeEn, rst);
     input rst;                              // Reset whole memory to 0
 
     // We cannot simulate 2**ADDR_LEN, so we choose a smaller instruction memory size
-    wire[$clog2(`DATA_MEM_SIZE):0] readAddr_trunc = 
-        readAddr[$clog2(`DATA_MEM_SIZE):0];
-    wire[$clog2(`DATA_MEM_SIZE):0] writeAddr_trunc = 
-        writeAddr[$clog2(`DATA_MEM_SIZE):0];
+    wire[$clog2(`DATA_MEM_SIZE):0] address_trunc = 
+        address[$clog2(`DATA_MEM_SIZE):0];
 
     reg[`MEM_CELL_SIZE-1:0] memory[0:`DATA_MEM_SIZE];
 
@@ -45,10 +41,10 @@ module mem_data (dataOut, readAddr, writeAddr, dataIn, clk, writeEn, rst);
                 memory[i] <= 0;
             end
         else if(writeEn) begin
-            {memory[writeAddr_trunc], memory[writeAddr_trunc+1]} <= dataIn;
+            {memory[address_trunc], memory[address_trunc+1]} <= dataIn;
             $display(".");
         end
     end
 
-    assign dataOut = {memory[readAddr_trunc], memory[readAddr_trunc+1]};
+    assign dataOut = {memory[address_trunc], memory[address_trunc+1]};
 endmodule
--- RiSC16/non_pipelined/testbenches/mem_data_tb.v
+++ RiSC16/non_pipelined/testbenches/mem_data_tb.v
@@ -2,15 +2,14 @@
 
 module mem_reg_tb;
     wire[`WORD_LEN-1:0]       dataOut;    // Data for reading
-    reg[`ADDR_LEN-1:0]        readAddr;   // Address for reading
-    reg[`ADDR_LEN-1:0]        writeAddr;  // Address for writing
+    reg[`ADDR_LEN-1:0]        address;   // Address for reading
     reg[`WORD_LEN-1:0]        dataIn;     // Data for writing
 
     reg clk;                              // Clock signal
     reg writeEn;                          // Active high signal for enabling write    
     reg rst;                              // Reset whole memory to 0
 
-    mem_data tud (dataOut, readAddr, writeAddr, dataIn, clk, writeEn, rst);
+    mem_data tud (dataOut, address, dataIn, clk, writeEn, rst);
 
     initial clk = 0;
     always  #1 clk = ~clk;
@@ -27,20 +26,20 @@ module mem_reg_tb;
         #2
         rst = 1'b0;
 
-        readAddr = 2;
+        address = 2;
         
         // Write to registers
         writeEn = 1'b1;
         for(i = 0; i < 8; i = i + 1) begin
             dataIn = i**2 + 10;
-            writeAddr = 2*i;
+            address = 2*i;
             #2;
         end
         writeEn = 1'b0;
         
         // Read from stored addresses
         for(i = 0; i < 8; i = i + 1) begin
-            readAddr = 2 * i;
+            address = 2 * i;
             #2;
         end
 
