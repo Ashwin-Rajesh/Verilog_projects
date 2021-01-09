3cf7228ac55abc3cf5d00bce79e6c3a8d06fbe83
Ashwin <aswinrajesh1@gmail.com>
2021-01-09 23:39:49 +0530

    increased instruction memory size

--- RiSC16/non_pipelined/defines.v
+++ RiSC16/non_pipelined/defines.v
@@ -5,7 +5,7 @@
 
 // Size of memory modules
 `define DATA_MEM_SIZE   500
-`define INSTR_MEM_SIZE  100
+`define INSTR_MEM_SIZE  500
 `define REG_FILE_SIZE   8
 `define REG_ADDR_LEN    3
 `define MEM_CELL_SIZE   8
